194d5a6ff286f7480d2d5220350af042301331c4
Tatiana Hodic <tatiana.hodic.miklos@gmail.com>
2022-09-20 14:50:58 +0200

    done

--- src/index.jsx
+++ src/index.jsx
@@ -6,23 +6,22 @@ import './style.css';
 const STEP_SIZE = 25;
 
 const App = () => {
-  const [posX, setPosX] = useState(100);
-  const [posY, setPosY] = useState(100);
-  const [orientation, setOrientation] = useState('right');
+  const [ladybugState, setLadybugState] = useState({
+    posX: 100,
+    posY: 100,
+    orientation: 'right',
+  });
   
   const handleKeyUp = ({ code }) => {
+    console.log(code);
     if (code === 'ArrowUp') {
-      setOrientation('up');
-      setPosX(posX - STEP_SIZE);
+      setLadybugState({...ladybugState, orientation: 'up', posX: ladybugState.posX - STEP_SIZE});
     } else if (code === 'ArrowLeft') {
-      setOrientation('left');
-      setPosY(posY - STEP_SIZE);
+      setLadybugState({...ladybugState, orientation: 'left', posY: ladybugState.posY - STEP_SIZE});
     } else if (code === 'ArrowRight') {
-      setOrientation('right');
-      setPosY(posY + STEP_SIZE);
+      setLadybugState({...ladybugState, orientation: 'right', posY: ladybugState.posY + STEP_SIZE});
     } else if (code === 'ArrowDown') {
-      setOrientation('down');
-      setPosX(posX + STEP_SIZE);
+      setLadybugState({...ladybugState, orientation: 'down', posX: ladybugState.posX + STEP_SIZE});
     }
   };
 
@@ -33,7 +32,7 @@ const App = () => {
       onKeyDown={handleKeyUp}
     >
       <header>Click anywhere to start the game</header>
-      <Ladybug posX={posX} posY={posY} orientation={orientation}/>
+      <Ladybug ladybugState={ladybugState}/>
     </div>
   );
 };
